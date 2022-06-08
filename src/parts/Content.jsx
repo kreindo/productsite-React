3048a853a7e195d3ba96da176a2df806dc7bd6e8
ahmadsan <ahmadsan4p@gmail.com>
2022-06-08 13:33:12 +0800

    added more illustrations

--- src/parts/Content.jsx
+++ src/parts/Content.jsx
@@ -5,13 +5,22 @@ import Typography from "@mui/material/Typography";
 import Desc from "../components/Desc";
 import Box from "@mui/material/Box";
 import Divider from "../components/Divider";
-import dCube from "../assets/images/1.png"
-import devGirl from '../assets/images/Coding1.png'
+import dCube from "../assets/images/1.png";
+import devGirl from "../assets/images/Coding1.png";
+import devBoy from '../assets/images/fixing-bugs.png'
+import devGirl2 from '../assets/images/modular-coding-application.png'
 
 function Content() {
   return (
     <Container>
-      <img className='slide-top1' style={{ width: '10em', display: 'absolute'}} src={dCube} alt="bg" />
+      <Box sx={{display: 'absolute'}}>
+        <img
+          className="slide-top1"
+          style={{ width: "10em", display: "absolute" }}
+          src={dCube}
+          alt="bg"
+        />
+      </Box>
       <Container
         maxWidth="md"
         sx={{ display: "flex", flexDirection: "column", alignItems: "center" }}
@@ -28,22 +37,26 @@ function Content() {
         <VPTransparent />
       </Container>
       <Divider />
-      <Container maxWidth='md' gutterTop gutterBottom sx={{ display: 'flex'}}>
-        <Desc title='Availability is our specialty'/>
+      <Container maxWidth="md" gutterTop gutterBottom sx={{ display: "flex" }}>
+        <Desc title="Availability is our specialty" />
         <Box>
-          <img style={{width: '200', height: 'auto'}} src={devGirl} alt="" />
+          <img style={{ width: "200", height: "auto" }} src={devGirl} alt="" />
         </Box>
       </Container>
-      <Container maxWidth='md' gutterTop gutterBottom sx={{ display: 'flex'}}>
+      <Container maxWidth="md" gutterTop gutterBottom sx={{ display: "flex" }}>
         <Box>
-          <img style={{width: '200', height: 'auto', margin: '0 10px'}} src={devGirl} alt="" />
+          <img
+            style={{ width: "200", height: "auto", margin: "0 10px" }}
+            src={devGirl2}
+            alt=""
+          />
         </Box>
-        <Desc title='Super() is weird'/>
+        <Desc title="Super() is weird" />
       </Container>
-      <Container maxWidth='md' gutterTop gutterBottom sx={{ display: 'flex'}}>
-        <Desc title='Stay connected'/>
+      <Container maxWidth="md" gutterTop gutterBottom sx={{ display: "flex" }}>
+        <Desc title="Stay connected" />
         <Box>
-          <img style={{width: '200', height: 'auto'}} src={devGirl} alt="" />
+          <img style={{ width: "200", height: "auto" }} src={devBoy} alt="" />
         </Box>
       </Container>
     </Container>
