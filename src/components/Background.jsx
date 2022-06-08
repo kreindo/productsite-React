21bb0f59187c3aabd7cdcf563b1d7949b0882e6d
ahmadsan <ahmadsan4p@gmail.com>
2022-06-08 08:52:20 +0800

    trying something

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -5,7 +5,7 @@ import dCube from '../assets/images/1.png'
 function Background() {
     return (
         <Box sx={{display: 'absolute'}}>
-            <img className='slide-top' style={{ width: '5em', display: 'absolute'}} src={dCube} alt="bg" />
+            <img className='slide-top' style={{ width: '5em', display: 'relative'}} src={dCube} alt="bg" />
         </Box>
     )
 }
